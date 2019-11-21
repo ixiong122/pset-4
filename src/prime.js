f3c0cf578d1ce02f2ba890734eec486c906c7fd5
ixiong122 <55449385+ixiong122@users.noreply.github.com>
2019-11-21 13:53:38 -0500

    Update prime.js

--- src/prime.js
+++ src/prime.js
@@ -6,25 +6,27 @@ let positiveInteger = 0;
 let isPrime = false;
 
 do {
-  const positiveInteger = readlineSync.question("Non-negative integer: ");
+   positiveInteger = readlineSync.question("Non-negative integer: ");
 } while (positiveInteger < MIN || positiveInteger > MAX || positiveInteger % 1 !== 0 || Number.isNaN(positiveInteger));
+
 if (positiveInteger == 1) {
-  console.log("Not Prime.");
   isPrime = false;
-} if (positiveInteger == 2){
-  console.log("Prime.");
+  // break;
+} else if (positiveInteger == 2){
   isPrime = true;
-}
-for (let i = 3; i < positiveInteger; i++) {
+  // break;
+} else {
+  for (let i = 2; i < positiveInteger; i++){
   if (positiveInteger % i == 0){
     isPrime = false;
-  break;
-} else {
-isPrime = true;
-} 
-if (isPrime = true){
-  console.log("Prime.");
-} else {
-  console.log("Not prime.");
+    // break;
+  } else {
+    isPrime = true;
+  }
 }
+}
+if (isPrime) {
+  console.log("Not prime.");
+} else {
+  console.log("Prime.");
 }
